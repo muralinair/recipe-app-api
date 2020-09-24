5db8e741c3c4c77605b3e05c8b7abfcba95679d2
course <mura.lee@hotmail.com>
2020-09-24 23:11:49 +0530

    changes no:2@

---
+++ app/app/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from app.calc import add,subtract
+
+class CalcTest(TestCase):
+    def test_add_numbers(self):
+        """Test for two numbers"""
+        self.assertEqual(add(3,8),11)
+
+    def test_subtract_numbers(self):
+        """test for subtractions"""
+        self.assertEqual(subtract(11,6),5)
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,21 +1,31 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import BaseUserManager
 
-class ModelTests(TestCase,BaseUserManager):
-    def test_create_user_with_email_successful(self):
+class ModelTest(TestCase):
+    def test_Create_user_with_email_successful(self):
         email="test@test.com"
-        #password="Testpass123"
-        password=self.make_random_password()
-        print("password generated: "+password)
+        password="Testpass123"
         user=get_user_model().objects.create_user(
             email=email,
             password=password
         )
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
-
+    
     def test_new_user_email_normalized(self):
-        email="test@TESTINGAPP.COM"
-        user=get_user_model().objects.create_user(email,'testing123')
+        email='test@TEST.COM'
+        user=get_user_model().objects.create_user(email,'1234')
         self.assertEqual(user.email,email.lower())
+    
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,"123")
+
+    def test_create_new_superuser(self):
+        user=get_user_model().objects.create_superuser(
+            'test"test.com',
+            "12345"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff) 
+            
